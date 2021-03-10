b2799833fb3e0e4afa02b1934d5d71998dc8c29e
koroko03 <korokonamae@gmail.com>
2021-03-10 12:26:35 +0700

    Perubahan struktur calculator

--- Calculator.cs
+++ Calculator.cs
@@ -4,4 +4,24 @@
         {
 		
         }
+		
+		static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
 	}
